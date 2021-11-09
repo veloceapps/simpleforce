1df608aeba2f9356bcdb86d428699aa722f105d6
vkorehov <vladislavs.korehovs@veloceapps.com>
2021-11-09 11:20:38 +0200

    addign support for metadata upload

---
+++ meta.go
@@ -0,0 +1,168 @@
+package simpleforce
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/pkg/errors"
+	"io"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"net/textproto"
+	"strings"
+	"time"
+)
+
+type MetaDeployResult struct {
+	Success             bool        `json:"success"`
+	ErrorStatusCode  string         `json:"errorStatusCode"`
+	ErrorMessage     string         `json:"errorMessage"`
+	Details          interface{}   `json:"details"`
+}
+
+type MetaDeployResponseResult struct {
+	ID               string        `json:"id"`
+	Success          bool          `json:"success"`
+	Done             bool          `json:"done"`
+	Status           string        `json:"status"`
+	ErrorStatusCode  string        `json:"errorStatusCode"`
+	ErrorMessage     string        `json:"errorMessage"`
+	Details          interface{}   `json:"details"`
+}
+
+type MetaDeployResponse struct {
+	ID             string                   `json:"id"`
+	DeployResult   MetaDeployResponseResult `json:"deployResult"`
+}
+
+
+// CreateScratch creates scratch with given OrgName
+func (c *Client) MetaDeploy(zip []byte, testLevel string) (*MetaDeployResult, error) {
+	if !c.isLoggedIn() {
+		return nil, ErrAuthentication
+	}
+
+	url := fmt.Sprintf("%s/services/data/v%s/metadata/deployRequest", c.instanceURL, DefaultAPIVersion)
+	// Prepare a form that you will submit to that URL.
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+	var fw io.Writer
+	var err error
+
+	// Add descriptor
+	sr := strings.NewReader(fmt.Sprintf(`{
+    "deployOptions" : {
+        "allowMissingFiles" : false,
+        "autoUpdatePackage" : false,
+        "checkOnly" : false,
+        "ignoreWarnings" : false,
+        "performRetrieve" : false,
+        "purgeOnDelete" : false,
+        "rollbackOnError" : false,
+        "runTests" : null,
+        "singlePackage" : true,
+        "testLevel" : "%s"
+    }
+}`, testLevel))
+	h := make(textproto.MIMEHeader)
+	h.Set("Content-Disposition",
+		fmt.Sprintf(`form-data; name="%s"`,
+			"json"))
+	h.Set("Content-Type", "application/json")
+	if fw, err = w.CreatePart(h); err != nil {
+		return &MetaDeployResult{Success: false}, err
+	}
+
+	if _, err := io.Copy(fw, sr); err != nil {
+		return &MetaDeployResult{Success: false}, err
+	}
+
+	// Add zip
+	br := bytes.NewReader(zip)
+
+	h = make(textproto.MIMEHeader)
+	h.Set("Content-Disposition",
+		fmt.Sprintf(`form-data; name="%s"; filename="%s"`,
+			"file", "deploy.zip"))
+	h.Set("Content-Type", "application/zip")
+	if fw, err = w.CreatePart(h); err != nil {
+		return &MetaDeployResult{Success: false}, err
+	}
+	if _, err := io.Copy(fw, br); err != nil {
+		return &MetaDeployResult{Success: false}, err
+	}
+
+	// Don't forget to close the multipart writer.
+	// If you don't close it, your request will be missing the terminating boundary.
+	w.Close()
+
+	// Now that you have a form, you can submit it to your handler.
+	req, err := http.NewRequest("POST", url, &b)
+	if err != nil {
+		return &MetaDeployResult{Success: false}, err
+	}
+	// Don't forget to set the content type, this will contain the boundary.
+	req.Header.Set("Content-Type", w.FormDataContentType())
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.sessionID))
+
+	// Submit the request
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return &MetaDeployResult{Success: false}, err
+	}
+	defer res.Body.Close()
+
+	// Check the response
+	if res.StatusCode != http.StatusCreated {
+		resBytes, _ := ioutil.ReadAll(res.Body)
+		log.Printf("Error from server: %s", string(resBytes))
+		return &MetaDeployResult{Success: false}, fmt.Errorf("bad status: %s", res.Status)
+	}
+	resBytes, _ := ioutil.ReadAll(res.Body)
+
+	var mr MetaDeployResponse
+	err = json.Unmarshal(resBytes, &mr)
+	if err != nil {
+		return &MetaDeployResult{Success: false}, err
+	}
+
+	maxWait := 120;
+	for {
+		maxWait--;
+		if maxWait <= 0 {
+			mr.DeployResult.Success = false
+			mr.DeployResult.ErrorMessage = "Timeout while waiting for result"
+			break;
+		}
+		if mr.DeployResult.Done {
+			break
+		}
+		url = fmt.Sprintf("%s/services/data/v%s/metadata/deployRequest/%s?includeDetails=true", c.instanceURL, DefaultAPIVersion, mr.ID)
+		//req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.sessionID))
+		resBytes, err = c.httpRequest("GET", url, nil)
+		if err != nil {
+			return &MetaDeployResult{Success: false}, err
+		}
+		err = json.Unmarshal(resBytes, &mr)
+		if err != nil {
+			return &MetaDeployResult{Success: false}, err
+		}
+		time.Sleep(1*time.Second)
+	}
+
+	if !mr.DeployResult.Success {
+		return &MetaDeployResult{Success: mr.DeployResult.Success,
+			ErrorMessage: mr.DeployResult.ErrorMessage,
+			ErrorStatusCode: mr.DeployResult.ErrorStatusCode,
+			Details: mr.DeployResult.Details,
+		}, errors.New("Deployment Failed")
+	}
+
+	return &MetaDeployResult{Success: mr.DeployResult.Success,
+		ErrorMessage: mr.DeployResult.ErrorMessage,
+		ErrorStatusCode: mr.DeployResult.ErrorStatusCode,
+		Details: mr.DeployResult.Details,
+	}, nil
+}
--- meta_test.go
+++ meta_test.go
@@ -3,238 +3,12 @@ package simpleforce
 import (
 	"archive/zip"
 	"bytes"
-	"errors"
 	"fmt"
-	"math/rand"
-	"strings"
+	"log"
+	"testing"
 )
 
-// ExecuteAnonymousResult is returned by ExecuteAnonymous function
-type CreateScratchResult struct {
-	Features            string      `json:"features"`
-	LoginURL            string      `json:"loginUrl"`
-	User                string      `json:"user"`
-	Pass                string      `json:"pass"`
-	AuthCode            string      `json:"authCode"`
-	Success             bool        `json:"success"`
-	ExceptionStackTrace interface{} `json:"exceptionStackTrace"`
-	ExceptionMessage    interface{} `json:"exceptionMessage"`
-}
-
-type RemoveScratchResult struct {
-	Success             bool        `json:"success"`
-	ExceptionStackTrace interface{} `json:"exceptionStackTrace"`
-	ExceptionMessage    interface{} `json:"exceptionMessage"`
-}
-
-var (
-	lowerCharSet   = "abcdedfghijklmnopqrst"
-	upperCharSet   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	specialCharSet = "!@#$%&*"
-	numberSet      = "0123456789"
-	allCharSet     = lowerCharSet + upperCharSet + specialCharSet + numberSet
-)
-
-func generatePassword(passwordLength, minSpecialChar, minNum, minUpperCase int) string {
-	var password strings.Builder
-
-	//Set special character
-	for i := 0; i < minSpecialChar; i++ {
-		random := rand.Intn(len(specialCharSet))
-		password.WriteString(string(specialCharSet[random]))
-	}
-
-	//Set numeric
-	for i := 0; i < minNum; i++ {
-		random := rand.Intn(len(numberSet))
-		password.WriteString(string(numberSet[random]))
-	}
-
-	//Set uppercase
-	for i := 0; i < minUpperCase; i++ {
-		random := rand.Intn(len(upperCharSet))
-		password.WriteString(string(upperCharSet[random]))
-	}
-
-	remainingLength := passwordLength - minSpecialChar - minNum - minUpperCase
-	for i := 0; i < remainingLength; i++ {
-		random := rand.Intn(len(allCharSet))
-		password.WriteString(string(allCharSet[random]))
-	}
-	inRune := []rune(password.String())
-	rand.Shuffle(len(inRune), func(i, j int) {
-		inRune[i], inRune[j] = inRune[j], inRune[i]
-	})
-	return string(inRune)
-}
-
-// HasScratch creates scratch with given OrgName
-func (client *Client) HasScratch(name string) (bool, error) {
-	if !client.isLoggedIn() {
-		return false, ErrAuthentication
-	}
-
-	// Query active org by OrgName first!
-	q := fmt.Sprintf("SELECT FIELDS(ALL) FROM ScratchOrgInfo WHERE OrgName = '%s' AND Status = 'Active' LIMIT 2", name)
-	result, err := client.Query(q)
-	if err != nil {
-		return false, err
-	}
-	if len(result.Records) > 0 {
-		if len(result.Records) > 1 {
-			return false, errors.New(fmt.Sprintf("More then one active org with OrgName: %s", name))
-		}
-		return true, nil
-	}
-	return false, nil
-}
-
-// CreateScratch creates scratch with given OrgName
-func (client *Client) CreateScratch(name string, adminEmail string, features string, phone string, countryName string) (*CreateScratchResult, error) {
-	if !client.isLoggedIn() {
-		return nil, ErrAuthentication
-	}
-
-	apexBodyTemplate:= `
-      ScratchOrgInfo newScratch = new ScratchOrgInfo (
-        OrgName = '%s',
-        Edition = 'Developer',
-        AdminEmail = '%s',
-        ConnectedAppConsumerKey = '%s',
-        ConnectedAppCallbackUrl = '%s',
-        DurationDays = 30,
-        Features = '%s'
-      );
-      insert(newScratch);
-    `
-	apexBody := fmt.Sprintf(apexBodyTemplate, name, adminEmail, DefaultClientID, DefaultRedirectURI, features)
-	_, err := client.ExecuteAnonymous(apexBody)
-	if err != nil {
-		return nil, err
-	}
-
-	// Query newly created Org
-	q := fmt.Sprintf("SELECT FIELDS(ALL) FROM ScratchOrgInfo WHERE OrgName = '%s' AND Status = 'Active' LIMIT 2", name)
-	result, err := client.Query(q)
-	if err != nil {
-		return nil, err
-	}
-	var output CreateScratchResult
-	if len(result.Records) > 1 {
-		return nil, errors.New(fmt.Sprintf("More then one active org with OrgName: %s", name))
-	}
-	if len(result.Records) == 0 {
-		return &CreateScratchResult{Success: false},
-			errors.New("Org Not Found after just created")
-	}
-
-	existingOrg := &result.Records[0]
-	output = CreateScratchResult{Success: true,
-		AuthCode: existingOrg.StringField("AuthCode"),
-		User: existingOrg.StringField("SignupUsername"),
-		LoginURL: existingOrg.StringField("LoginUrl"),
-		Features: existingOrg.StringField("Features"),
-	}
-
-	scratchClient := NewClient(output.LoginURL, DefaultClientID, DefaultAPIVersion)
-	err = scratchClient.LoginWithAuthCode(output.LoginURL, output.AuthCode)
-	if err != nil {
-		return &CreateScratchResult{Success: false},
-			errors.New(fmt.Sprintf("AuthCode auth failed just after org creation: %s", err))
-	}
-
-	// Set Scratch Password to be Random strong pass
-	pass := generatePassword(16, 2,2,2)
-	apexBodyTemplate = `
-      System.setPassword(userInfo.getUserId(),'%s');
-    `
-	apexBody = fmt.Sprintf(apexBodyTemplate, pass)
-	_, err = scratchClient.ExecuteAnonymous(apexBody)
-	if err != nil {
-		return &CreateScratchResult{Success: false}, err
-	}
-	output.Pass = pass;
-
-	// Set Phone number in scratch to avoid prompts on UI
-	apexBodyTemplate = `
-      String phoneNumber = '%s';
-      String countryName = '%s';
-      String userId = UserInfo.getUserId();
-      User user = [SELECT Id, Name,MobilePhone FROM User WHERE Id =: userId LIMIT 1];
-
-      user.Country = countryName;
-      user.MobilePhone = phoneNumber;
-
-      update user;
-    `
-	apexBody = fmt.Sprintf(apexBodyTemplate, phone, countryName)
-	_, err = scratchClient.ExecuteAnonymous(apexBody)
-	if err != nil {
-		return &CreateScratchResult{Success: false}, err
-	}
-
-	// APPLY Security settings to allow authorizing without 2FA
-	/* zip layout:
-	package.xml
-	settings/Security.settings
-	*/
-	buf := new(bytes.Buffer)
-	// Create a new zip archive.
-	zipWriter := zip.NewWriter(buf)
-
-	// Add some files to the archive.
-	var files = []struct {
-		Name, Body string
-	}{
-		{"package.xml", ScratchPackageXML},
-		{"settings/Security.settings", ScratchSecuritySettingsMeta},
-	}
-	for _, file := range files {
-		zipFile, err := zipWriter.Create(file.Name)
-		if err != nil {
-			return &CreateScratchResult{Success: false}, err
-		}
-		_, err = zipFile.Write([]byte(file.Body))
-		if err != nil {
-			return &CreateScratchResult{Success: false}, err
-		}
-	}
-
-	// Make sure to check the error on Close.
-	err = zipWriter.Close()
-	if err != nil {
-		return &CreateScratchResult{Success: false}, err
-	}
-
-	_, err = scratchClient.MetaDeploy(buf.Bytes(), "NoTestRun")
-	if err != nil {
-		return &CreateScratchResult{Success: false}, err
-	}
-
-	return &output, nil
-}
-
-// RemoveScratch creates or retrieves scratch details based on OrgName
-func (client *Client) RemoveScratch(name string) (*RemoveScratchResult, error) {
-	if !client.isLoggedIn() {
-		return nil, ErrAuthentication
-	}
-
-	apexBodyTemplate:= `
-      ScratchOrgInfo[] orgs = [SELECT Id FROM ScratchOrgInfo WHERE OrgName = '%s' AND Status = 'Active' LIMIT 10];
-      delete orgs;
-    `
-	apexBody := fmt.Sprintf(apexBodyTemplate, name)
-
-	_, err := client.ExecuteAnonymous(apexBody)
-	if err != nil {
-		return nil, err
-	}
-
-	return &RemoveScratchResult{Success: true}, nil
-}
-
-const ScratchSecuritySettingsMeta = `<?xml version="1.0" encoding="UTF-8"?>
+const SecuritySettingsMeta = `<?xml version="1.0" encoding="UTF-8"?>
 <SecuritySettings xmlns="http://soap.sforce.com/2006/04/metadata">
     <canUsersGrantLoginAccess>true</canUsersGrantLoginAccess>
     <enableAdminLoginAsAnyUser>false</enableAdminLoginAsAnyUser>
@@ -1579,7 +1353,7 @@ const ScratchSecuritySettingsMeta = `<?xml version="1.0" encoding="UTF-8"?>
     </singleSignOnSettings>
 </SecuritySettings>`
 
-const ScratchPackageXML = `<?xml version="1.0" encoding="UTF-8"?>
+const PackageXML = `<?xml version="1.0" encoding="UTF-8"?>
 <Package xmlns="http://soap.sforce.com/2006/04/metadata">
     <types>
         <members>Security</members>
@@ -1587,3 +1361,53 @@ const ScratchPackageXML = `<?xml version="1.0" encoding="UTF-8"?>
     </types>
     <version>53.0</version>
 </Package>`
+
+//https://developer.salesforce.com/docs/atlas.en-us.api_meta.meta/api_meta/meta_rest_deploy.htm
+
+func TestClient_MetaDeploy(t *testing.T) {
+	client := requireClient(t, true)
+	/* zip layout:
+	package.xml
+	settings/Security.settings
+	*/
+	buf := new(bytes.Buffer)
+	// Create a new zip archive.
+	zipWriter := zip.NewWriter(buf)
+
+	// Add some files to the archive.
+	var files = []struct {
+		Name, Body string
+	}{
+		{"package.xml", PackageXML},
+		{"settings/Security.settings", SecuritySettingsMeta},
+	}
+	for _, file := range files {
+		zipFile, err := zipWriter.Create(file.Name)
+		if err != nil {
+			log.Println(err)
+		}
+		_, err = zipFile.Write([]byte(file.Body))
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	// Make sure to check the error on Close.
+	err := zipWriter.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	//write the zipped file to the disk
+	//ioutil.WriteFile("/tmp/deploy.zip", buf.Bytes(), 0777)
+
+	result, err := client.MetaDeploy(buf.Bytes(), "NoTestRun")
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		t.FailNow()
+	}
+	if !result.Success {
+		t.FailNow()
+	}
+	fmt.Printf("Result: %+v\n", result)
+}
+
